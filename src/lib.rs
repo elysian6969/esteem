fd4d44d71f5cd27e90c5825fc4f70be5224d0b67
elysian <tsujianii@tuta.io>
2022-06-12 00:02:03 +0800

    wip p2

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
+#![allow(dead_code)]
 #![feature(asm_sym)]
+#![feature(linkage)]
+#![feature(naked_functions)]
 #![feature(used_with_arg)]
 
-use std::collections::{BTreeMap, HashMap};
+use std::arch::asm;
 use std::ffi::{OsStr, OsString};
 use std::os::unix::process::CommandExt;
 use std::path::{Path, PathBuf};
@@ -17,63 +20,40 @@ const fn new_path(path: &'static str) -> &'static Path {
     unsafe { &*(path as *const str as *const Path) }
 }
 
-fn var_os<K: AsRef<OsStr>>(env: &HashMap<OsString, OsString>, key: K) -> OsString {
-    env.get(key.as_ref())
-        .map(|env| env.clone())
-        .unwrap_or_default()
-}
-
-fn var_split<K: AsRef<OsStr>>(env: &HashMap<OsString, OsString>, key: K) -> Vec<PathBuf> {
-    std::env::split_paths(&var_os(env, key)).collect()
-}
-
-//const PREFIX: &Path = new_path(env!("CARGO_MANIFEST_DIR"));
 const PREFIX: &Path = new_path("/usr/lib/esteem");
 
 fn print_option(option: &str) {
     println!("esteem | enabled \x1b[38;5;1m{option}\x1b[m");
 }
 
-use std::borrow::Cow;
-
-struct Args<'a>(Vec<Cow<'a, str>>);
-
-impl<'a> Args<'a> {
-    pub const fn new() -> Self {
-        Self(Vec::new())
-    }
-
-    pub fn push<S>(&mut self, arg: S) -> &mut Self
-    where
-        S: Into<Cow<'a, str>>,
-    {
-        self.0.push(arg.into());
-        self
-    }
-}
-
-// this is called by glibc after the library is loaded into a process
+// .init_array entries are invoked by glibc
 #[link_section = ".init_array"]
 #[used(linker)]
 static BOOTSTRAP: unsafe extern "C" fn() = bootstrap;
 
-// this tricks glibc into thinking this is an executable binary
-#[link_section = ".interp"]
-#[used(linker)]
-static INTERPRETER: [u8; 19] = *b"/lib/ld-linux.so.2\0";
-
+// as above
 #[link_section = ".text.startup"]
 pub unsafe extern "C" fn bootstrap() {
     println!("esteem | bootstrap");
 
     let program = std::env::args_os().next();
 
-    println!("{program:?}");
+    println!("esteem | {program:?}");
 }
 
+// we'd like to be interpreted
+#[link_section = ".interp"]
+#[used(linker)]
+static INTERPRETER: [u8; 19] = *b"/lib/ld-linux.so.2\0";
+
+// link with `-e_start` to redirect the entry to here
+// regular start procedures are broken
 #[no_mangle]
-pub unsafe extern "C" fn entry() {
-    core::arch::asm!(
+//#[naked]
+pub unsafe extern "C" fn _start() -> ! {
+    asm!(
+        // mark outermost stack frame by zeroing ebp
+        "xor ebp, ebp",
         // pop top of stack
         "pop esi",
         // move kernel block pointer to esp
@@ -82,113 +62,78 @@ pub unsafe extern "C" fn entry() {
         "and esp, -16",
         // call setup
         "call {}",
-        sym setup,
+        sym pre_main,
+        options(noreturn),
     );
 }
 
-#[derive(Debug)]
-#[repr(C)]
-struct Inner {
-    argc: isize,
-    argv: *const u8,
+#[inline]
+unsafe fn init_rust_args(argc: i32, argv: *const *const u8) {
+    use std::ptr;
+
+    // https://github.com/rust-lang/rust/blob/master/library/std/src/sys/unix/args.rs#L110
+    extern "C" {
+        #[link_name = "_ZN3std3sys4unix4args3imp15ARGV_INIT_ARRAY17h2b585808917e1adfE"]
+        static ARGV_INIT_ARRAY:
+            unsafe extern "C" fn(argc: i32, argv: *const *const u8, envp: *const *const u8);
+    }
+
+    (ARGV_INIT_ARRAY)(argc, argv, ptr::null());
 }
 
-#[derive(Debug)]
-#[repr(C)]
-struct KernelInfo {
-    inner: &'static Inner,
+unsafe extern "C" fn pre_main(info: *const u8) {
+    #[repr(C)]
+    struct Info {
+        argc: isize,
+        argv: *const u8,
+    }
+
+    let info = info.cast::<Info>();
+    let argc = (*info).argc;
+    let argv = &(*info).argv;
+
+    // fix `std::env::args()`
+    init_rust_args(argc as i32, argv);
+
+    main();
+
+    std::process::exit(0);
 }
 
-unsafe fn slice<'a, T>(address: *const T) -> &'a [T]
-where
-    T: PartialEq,
-    T: 'a,
-{
-    let mut end = address;
+fn main() {
+    use std::env;
 
-    while *end != core::mem::MaybeUninit::<T>::zeroed().assume_init() {
-        end = end.add(1);
-    }
-
-    let len = end.offset_from(address);
-
-    core::slice::from_raw_parts(address, len as usize)
-}
-
-use std::os::unix::ffi::OsStringExt;
-
-impl KernelInfo {
-    unsafe fn raw_args(&self) -> &[*const u8] {
-        let args = &self.inner.argv as *const *const u8;
-        let args = slice(args);
-
-        args
-    }
-
-    unsafe fn raw_envs(&self) -> &[*const u8] {
-        let args = self.raw_args();
-        let envs = args.as_ptr().add(args.len()).add(1);
-        let envs = slice(envs);
-
-        envs
-    }
-
-    pub fn args(&self) -> Vec<OsString> {
-        unsafe {
-            let args = self.raw_args();
-            let args = args.iter().map(|arg| {
-                let arg = slice(*arg);
-                let arg = arg.into();
-
-                OsStringExt::from_vec(arg)
-            });
-
-            args.collect()
-        }
-    }
-
-    pub fn envs(&self) -> HashMap<OsString, OsString> {
-        unsafe {
-            let envs = self.raw_envs();
-            let envs = envs.iter().map(|env| unsafe {
-                let env = slice(*env);
-                let mut parts = env.split(|byte| *byte == b'=');
-
-                let key = parts.next().unwrap_unchecked().into();
-                let val = parts.next().unwrap_unchecked().into();
-
-                let key = OsStringExt::from_vec(key);
-                let val = OsStringExt::from_vec(val);
-
-                (key, val)
-            });
-
-            envs.collect()
-        }
-    }
-}
-
-use clap::Parser;
-
-unsafe extern "C" fn setup(info: KernelInfo) {
-    let args = info.args();
-    let envs = info.envs();
-
-    let home = envs
-        .get(std::ffi::OsStr::new("HOME"))
+    let home_dir = env::var_os("HOME")
         .map(PathBuf::from)
         .unwrap_or_else(|| PathBuf::from("/"));
 
-    let data_dir = envs
-        .get(std::ffi::OsStr::new("XDG_DATA_HOME"))
+    let data_dir = env::var_os("XDG_DATA_HOME")
         .map(PathBuf::from)
-        .unwrap_or_else(|| home.join(".local/share"));
+        .unwrap_or_else(|| home_dir.join(".local/share"));
 
     let data_dir = data_dir.join("esteem");
 
-    let options = options::Options::parse_from(args.clone());
+    let options = options::Options::parse();
 
-    if envs.contains_key(std::ffi::OsStr::new(key::ESTEEM_LOAD_UI)) {
+    if env::var_os(key::ESTEEM_LOAD_UI).is_some() {
+        use std::borrow::Cow;
+
+        struct Args<'a>(Vec<Cow<'a, str>>);
+
+        impl<'a> Args<'a> {
+            pub const fn new() -> Self {
+                Self(Vec::new())
+            }
+
+            pub fn push<S>(&mut self, arg: S) -> &mut Self
+            where
+                S: Into<Cow<'a, str>>,
+            {
+                self.0.push(arg.into());
+                self
+            }
+        }
+
         let mut args = Args::new();
 
         // crash reporter
@@ -201,8 +146,6 @@ unsafe extern "C" fn setup(info: KernelInfo) {
         args.push(arg::NO_CRASH_MONITOR);
         // definitely dont need this
         args.push(arg::NO_INTRO);
-
-        println!("esteem | options");
 
         if options.devtools {
             print_option("devtools");
@@ -250,29 +193,36 @@ unsafe extern "C" fn setup(info: KernelInfo) {
         ui.main(args.0.iter().map(|s| s.as_ref()));
     } else {
         let i686 = PREFIX.join("i686");
-        let esteem = i686.join("esteem");
+        let esteem = i686.join("libesteem.so");
 
-        let mut path = var_split(&envs, key::PATH);
-        let mut ld_library_path = var_split(&envs, key::LD_LIBRARY_PATH);
+        fn var_os<K: AsRef<OsStr>>(key: K) -> OsString {
+            env::var_os(key).unwrap_or_default()
+        }
+
+        fn var_split<K: AsRef<OsStr>>(key: K) -> Vec<PathBuf> {
+            env::split_paths(&var_os(key)).collect()
+        }
+
+        let mut path = var_split(key::PATH);
+        let mut ld_library_path = var_split(key::LD_LIBRARY_PATH);
 
         path.insert(0, i686.clone());
         ld_library_path.insert(0, i686);
 
-        let path = std::env::join_paths(path).expect("PATH");
-        let ld_library_path = std::env::join_paths(ld_library_path).expect("LD_LIBRARY_PATH");
+        let path = env::join_paths(path).expect("PATH");
+        let ld_library_path = env::join_paths(ld_library_path).expect("LD_LIBRARY_PATH");
 
-        if let Some(program) = args.get(0) {
+        let mut args = env::args_os();
+
+        if let Some(program) = args.next() {
             let mut command = Command::new(program);
 
             // we set SYSTEM_LD_LIBRARY_PATH and SYSTEM_PATH because steamui.so does the following:
             //  __stream = popen("LD_LIBRARY_PATH=\"$SYSTEM_LD_LIBRARY_PATH\" PATH=\"$SYSTEM_PATH\" lspci -mm -n", "r");
 
-            let home_dir = home;
-            let data_dir = data_dir;
-
             command
                 .arg0(esteem)
-                .args(args.get(2))
+                .args(args.next())
                 .current_dir(&PREFIX)
                 .env(key::ESTEEM_LOAD_UI, "1")
                 .env(key::LD_LIBRARY_PATH, &ld_library_path)
